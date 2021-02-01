ddb8a067af98cefb1939bf635110bd59ee6c40af
lundih[B <lundih27@gmail.com>
2021-02-02 00:44:14 +0300

    feat: Use Webclient

--- movie-catalogue/src/main/java/com/lundih/moviecatalogue/MovieCatalogueApplication.java
+++ movie-catalogue/src/main/java/com/lundih/moviecatalogue/MovieCatalogueApplication.java
@@ -2,6 +2,8 @@ package com.lundih.moviecatalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogueApplication {
@@ -10,4 +12,10 @@ public class MovieCatalogueApplication {
 		SpringApplication.run(MovieCatalogueApplication.class, args);
 	}
 
+	// There is already a bean of type Builder in the MovieCatalogueController class
+	// Need to add a name qualifier to differentiate them
+	@Bean(name = "WebClientBuilder")
+	public WebClient.Builder WebClientBuilder() {
+		return WebClient.builder();
+	}
 }
